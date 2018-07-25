fd07c0015b77f6a817f7bcc0d8f1914bb2d94099
= <methawee.a@ku.th>
2018-07-26 01:53:01 +0200

    Refactor PySpark

--- pyspark/hour_pspark.py
+++ pyspark/hour_pspark.py
@@ -1,6 +1,5 @@
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
-from functools import reduce
 
 def init():
         conf = SparkConf().setAppName('findpats').setMaster('local')
@@ -10,24 +9,59 @@ def init():
 sc, sqlContext = init()
 
 # load file from csv
-gps = sqlContext.read.load('./data/gps_hour_data/22/gps_22_day1/part-00000-36e8eed7-800e-447a-b58d-d022803e328a-c000.csv', 
+gps = sqlContext.read.load('./data/raw_data/11 a 20/11-15/gps_20161115',
                           format='com.databricks.spark.csv', 
-                          inferSchema='true', sep='|')
-
-
+                          inferSchema='true')
 oldrows = gps.schema.names
-newrows = ['vehicle_id', 'order_id', 
-                'universal_time', 'longitude', 'latitude']
-gps = reduce(lambda gps, idx: gps.withColumnRenamed(
-                oldrows[idx], newrows[idx]), range(len(oldrows)), gps)   
-
-gps.printSchema()
-
+newrows = ['vehicle_id', 'order_id', 'universal_time', 'longitude', 'latitude']
+from functools import reduce
+gps = reduce(lambda gps, idx: gps.withColumnRenamed(oldrows[idx], newrows[idx]), range(len(oldrows)), gps)
 gps.registerTempTable('gps')
 
-# query 
-gps_1 = sqlContext.sql("SELECT * FROM gps WHERE universal_time = 1")
-gps_1.registerTempTable('gps_1')
+# Create zones
+gps_zone1 = sqlContext.sql('SELECT * FROM gps WHERE longitude >= CAST(104.0422 AS DOUBLE) AND longitude <= CAST(104.05967 AS DOUBLE) AND latitude >= CAST(30.7090575 AS DOUBLE) AND latitude <= CAST(30.72774 AS DOUBLE)')
+gps_zone1.repartition(1).write.csv("./zone/day15_zone1_20/gps_zone1", sep="|")
+gps_zone2 = sqlContext.sql('SELECT * FROM gps WHERE longitude >= CAST(104.05967 AS DOUBLE) AND longitude <= CAST(104.07714 AS DOUBLE) AND latitude >= CAST(30.7090575 AS DOUBLE) AND latitude <= CAST(30.72774 AS DOUBLE)')
+gps_zone2.repartition(1).write.csv("./zone/day15_zone1_20/gps_zone2", sep="|")
+gps_zone3 = sqlContext.sql('SELECT * FROM gps WHERE longitude >= CAST(104.07714 AS DOUBLE) AND longitude <= CAST(104.09461 AS DOUBLE) AND latitude >= CAST(30.7090575 AS DOUBLE) AND latitude <= CAST(30.72774 AS DOUBLE)')
+gps_zone3.repartition(1).write.csv("./zone/day15_zone1_20/gps_zone3", sep="|")
 
-# write data into csv file
-gps_1.repartition(1).write.csv("gps_1_day3", sep="|")
+gps_zone4 = sqlContext.sql('SELECT * FROM gps WHERE longitude >= CAST(104.09461 AS DOUBLE) AND longitude <= CAST(104.11208 AS DOUBLE) AND latitude >= CAST(30.7090575 AS DOUBLE) AND latitude <= CAST(30.72774 AS DOUBLE)')
+gps_zone4.repartition(1).write.csv("./zone/day15_zone1_20/gps_zone4", sep="|")
+gps_zone5 = sqlContext.sql('SELECT * FROM gps WHERE longitude >= CAST(104.11208 AS DOUBLE) AND longitude <= CAST(104.12955 AS DOUBLE) AND latitude >= CAST(30.7090575 AS DOUBLE) AND latitude <= CAST(30.72774 AS DOUBLE)')
+gps_zone5.repartition(1).write.csv("./zone/day15_zone1_20/gps_zone5", sep="|")
+gps_zone6 = sqlContext.sql('SELECT * FROM gps WHERE longitude >= CAST(104.0422 AS DOUBLE) AND longitude <= CAST(104.05967 AS DOUBLE) AND latitude >= CAST(30.690375 AS DOUBLE) AND latitude <= CAST(30.7090575 AS DOUBLE)')
+gps_zone6.repartition(1).write.csv("./zone/day15_zone1_20/gps_zone6", sep="|")
+
+gps_zone7 = sqlContext.sql('SELECT * FROM gps WHERE longitude >= CAST(104.05967 AS DOUBLE) AND longitude <= CAST(104.07714 AS DOUBLE) AND latitude >= CAST(30.690375 AS DOUBLE) AND latitude <= CAST(30.7090575 AS DOUBLE)')
+gps_zone7.repartition(1).write.csv("./zone/day15_zone1_20/gps_zone7", sep="|")
+gps_zone8 = sqlContext.sql('SELECT * FROM gps WHERE longitude >= CAST(104.07714 AS DOUBLE) AND longitude <= CAST(104.09461 AS DOUBLE) AND latitude >= CAST(30.690375 AS DOUBLE) AND latitude <= CAST(30.7090575 AS DOUBLE)')
+gps_zone8.repartition(1).write.csv("./zone/day15_zone1_20/gps_zone8", sep="|")
+gps_zone9 = sqlContext.sql('SELECT * FROM gps WHERE longitude >= CAST(104.09461 AS DOUBLE) AND longitude <= CAST(104.11208 AS DOUBLE) AND latitude >= CAST(30.690375 AS DOUBLE) AND latitude <= CAST(30.7090575 AS DOUBLE)')
+gps_zone9.repartition(1).write.csv("./zone/day15_zone1_20/gps_zone9", sep="|")
+
+gps_zone10 = sqlContext.sql('SELECT * FROM gps WHERE longitude >= CAST(104.11208 AS DOUBLE) AND longitude <= CAST(104.12955 AS DOUBLE) AND latitude >= CAST(30.690375 AS DOUBLE) AND latitude <= CAST(30.7090575 AS DOUBLE)')
+gps_zone10.repartition(1).write.csv("./zone/day15_zone1_20/gps_zone10", sep="|")
+gps_zone11 = sqlContext.sql('SELECT * FROM gps WHERE longitude >= CAST(104.0422 AS DOUBLE) AND longitude <= CAST(104.05967 AS DOUBLE) AND latitude >= CAST(30.6716925 AS DOUBLE) AND latitude <= CAST(30.690375 AS DOUBLE)')
+gps_zone11.repartition(1).write.csv("./zone/day15_zone1_20/gps_zone11", sep="|")
+gps_zone12 = sqlContext.sql('SELECT * FROM gps WHERE longitude >= CAST(104.05967 AS DOUBLE) AND longitude <= CAST(104.07714 AS DOUBLE) AND latitude >= CAST(30.6716925 AS DOUBLE) AND latitude <= CAST(30.690375 AS DOUBLE)')
+gps_zone12.repartition(1).write.csv("./zone/day15_zone1_20/gps_zone12", sep="|")
+
+gps_zone13 = sqlContext.sql('SELECT * FROM gps WHERE longitude >= CAST(104.07714 AS DOUBLE) AND longitude <= CAST(104.09461 AS DOUBLE) AND latitude >= CAST(30.6716925 AS DOUBLE) AND latitude <= CAST(30.690375 AS DOUBLE)')
+gps_zone13.repartition(1).write.csv("./zone/day15_zone1_20/gps_zone13", sep="|")
+gps_zone14 = sqlContext.sql('SELECT * FROM gps WHERE longitude >= CAST(104.09461 AS DOUBLE) AND longitude <= CAST(104.11208 AS DOUBLE) AND latitude >= CAST(30.6716925 AS DOUBLE) AND latitude <= CAST(30.690375 AS DOUBLE)')
+gps_zone14.repartition(1).write.csv("./zone/day15_zone1_20/gps_zone14", sep="|")
+gps_zone15 = sqlContext.sql('SELECT * FROM gps WHERE longitude >= CAST(104.11208 AS DOUBLE) AND longitude <= CAST(104.12955 AS DOUBLE) AND latitude >= CAST(30.6716925 AS DOUBLE) AND latitude <= CAST(30.690375 AS DOUBLE)')
+gps_zone15.repartition(1).write.csv("./zone/day15_zone1_20/gps_zone15", sep="|")
+
+gps_zone16 = sqlContext.sql('SELECT * FROM gps WHERE longitude >= CAST(104.0422 AS DOUBLE) AND longitude <= CAST(104.05967 AS DOUBLE) AND latitude >= CAST(30.65301 AS DOUBLE) AND latitude <= CAST(30.6716925 AS DOUBLE)')
+gps_zone16.repartition(1).write.csv("./zone/day15_zone1_20/gps_zone16", sep="|")
+gps_zone17 = sqlContext.sql('SELECT * FROM gps WHERE longitude >= CAST(104.05967 AS DOUBLE) AND longitude <= CAST(104.07714 AS DOUBLE) AND latitude >= CAST(30.65301 AS DOUBLE) AND latitude <= CAST(30.6716925 AS DOUBLE)')
+gps_zone17.repartition(1).write.csv("./zone/day15_zone1_20/gps_zone17", sep="|")
+gps_zone18 = sqlContext.sql('SELECT * FROM gps WHERE longitude >= CAST(104.07714 AS DOUBLE) AND longitude <= CAST(104.09461 AS DOUBLE) AND latitude >= CAST(30.65301 AS DOUBLE) AND latitude <= CAST(30.6716925 AS DOUBLE)')
+gps_zone18.repartition(1).write.csv("./zone/day15_zone1_20/gps_zone18", sep="|")
+
+gps_zone19 = sqlContext.sql('SELECT * FROM gps WHERE longitude >= CAST(104.09461 AS DOUBLE) AND longitude <= CAST(104.11208 AS DOUBLE) AND latitude >= CAST(30.65301 AS DOUBLE) AND latitude <= CAST(30.6716925 AS DOUBLE)')
+gps_zone19.repartition(1).write.csv("./zone/day15_zone1_20/gps_zone19", sep="|")
+gps_zone20 = sqlContext.sql('SELECT * FROM gps WHERE longitude >= CAST(104.11208 AS DOUBLE) AND longitude <= CAST(104.12955 AS DOUBLE) AND latitude >= CAST(30.65301 AS DOUBLE) AND latitude <= CAST(30.6716925 AS DOUBLE)')
+gps_zone20.repartition(1).write.csv("./zone/day15_zone1_20/gps_zone20", sep="|")
